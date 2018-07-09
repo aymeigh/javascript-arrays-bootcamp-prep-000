5baa56b8cddbeaf0af9a060e8695e746ca220ec7
Amy Zhou <aymeighz@gmail.com>
2018-07-09 19:52:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,7 +38,7 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.pop(-1);
+  return array.pop();
 }
 
 function removeElementFromEndOfArray(array) {
